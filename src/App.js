89777a1209a6b41a58e7a776b9d7240634b5364b
jql6 <jql6@sfu.ca>
2021-06-12 23:07:52 -0700

    Render Fetch Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import './App.css';
+import React, { Component } from "react";
+import FetchComponent from "./components/FetchComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <FetchComponent title="Fetch API" />
+      </div>
+    );
+  }
 }
 
 export default App;
