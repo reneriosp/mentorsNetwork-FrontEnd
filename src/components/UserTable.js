60a98d5505768d03cf089e2d38dc9adb900c6813
jql6 <jql6@sfu.ca>
2021-06-16 15:40:51 -0700

    Add loading indicator for async fetch

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -18,12 +18,18 @@ class UserTable extends Component {
           roles: null,
         },
       },
+      loading: false,
     };
   }
 
   async loadData() {
+    // Show that the async fetch is loading
+    this.setState({ loading: true });
+
     this.setState({
+      // When the data is obtained, remove loading indicator
       data: await fetchJSON(),
+      loading: false,
     });
   }
 
@@ -32,32 +38,36 @@ class UserTable extends Component {
       <div>
         <h1>{this.props.title}</h1>
         <button onClick={this.loadData}>Get data</button>
-        <table border={2} cellPadding={5}>
-          <thead>
-            <tr>
-              {Object.keys(this.state.data).map((element) => {
-                return Object.keys(this.state.data[element]).map(
-                  (element2, key) => {
-                    return <th key={key}>{element2}</th>;
-                  }
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              {Object.keys(this.state.data).map((element) => {
-                return Object.keys(this.state.data[element]).map(
-                  (element2, key) => {
-                    return (
-                      <td key={key}>{this.state.data[element][element2]}</td>
-                    );
-                  }
-                );
-              })}
-            </tr>
-          </tbody>
-        </table>
+        {this.state.loading ? (
+          <p>Loading...</p>
+        ) : (
+          <table border={2} cellPadding={5}>
+            <thead>
+              <tr>
+                {Object.keys(this.state.data).map((element) => {
+                  return Object.keys(this.state.data[element]).map(
+                    (element2, key) => {
+                      return <th key={key}>{element2}</th>;
+                    }
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {Object.keys(this.state.data).map((element) => {
+                  return Object.keys(this.state.data[element]).map(
+                    (element2, key) => {
+                      return (
+                        <td key={key}>{this.state.data[element][element2]}</td>
+                      );
+                    }
+                  );
+                })}
+              </tr>
+            </tbody>
+          </table>
+        )}
       </div>
     );
   }
