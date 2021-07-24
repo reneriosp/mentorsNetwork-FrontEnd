1b2f7295f7c4b09c720d80f4c8c7bbaee07c569d
jql6 <jql6@sfu.ca>
2021-07-23 21:41:51 -0700

    Convert into react hooks

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,76 +1,53 @@
-import React, { Component } from "react";
+// UserTable.js
+import React, { useState } from "react";
 import fetchJSON from "../functions/fetchJSON";
 
-class UserTable extends Component {
-  constructor(props) {
-    super(props);
+function UserTable(props) {
+  /* Placeholder table until we load the data */
+  const [data, setData] = useState({
+    identity: { id: null, login: null },
+    permissions: { roles: null },
+  });
+  const [loading, setLoading] = useState(false);
 
-    this.loadData = this.loadData.bind(this);
-
-    /* Placeholder table until we load the data */
-    this.state = {
-      data: {
-        identity: {
-          id: null,
-          login: null,
-        },
-        permissions: {
-          roles: null,
-        },
-      },
-      loading: false,
-    };
+  async function loadData() {
+    setLoading(true);
+    // Downloading json
+    setData(await fetchJSON());
+    // When download is finished, it's no longer loading
+    setLoading(false);
   }
 
-  async loadData() {
-    // Show that the async fetch is loading
-    this.setState({ loading: true });
-
-    this.setState({
-      // When the data is obtained, remove loading indicator
-      data: await fetchJSON(),
-      loading: false,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <button onClick={this.loadData}>Get data</button>
-        {this.state.loading ? (
-          <p>Loading...</p>
-        ) : (
-          <table border={2} cellPadding={5}>
-            <thead>
-              <tr>
-                {Object.keys(this.state.data).map((element) => {
-                  return Object.keys(this.state.data[element]).map(
-                    (element2, key) => {
-                      return <th key={key}>{element2}</th>;
-                    }
-                  );
-                })}
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                {Object.keys(this.state.data).map((element) => {
-                  return Object.keys(this.state.data[element]).map(
-                    (element2, key) => {
-                      return (
-                        <td key={key}>{this.state.data[element][element2]}</td>
-                      );
-                    }
-                  );
-                })}
-              </tr>
-            </tbody>
-          </table>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <button onClick={loadData}>Get data</button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table border={2} cellPadding={5}>
+          <thead>
+            <tr>
+              {Object.keys(data).map((element) => {
+                return Object.keys(data[element]).map((element2, key) => {
+                  return <th key={key}>{element2}</th>;
+                });
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {Object.keys(data).map((element) => {
+                return Object.keys(data[element]).map((element2, key) => {
+                  return <td key={key}>{data[element][element2]}</td>;
+                });
+              })}
+            </tr>
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
 export default UserTable;
