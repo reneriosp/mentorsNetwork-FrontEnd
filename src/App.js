d3d1a703586449e74dbac58e0cbcb1083b902603
German Alvarez <germanalvarez14@gmail.com>
2021-09-06 20:38:39 -0700

    made basic react router setup and created visuals for user info forms

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import "./App.css";
 import MainNavigation from "./components/layout/MainNavigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Auth from "./pages/Auth";
+import { Route, Switch } from "react-router";
+import Name from "./pages/UserInfo/Name";
+import Info from "./pages/UserInfo/Info";
 
 function App() {
   return (
     <div>
       <MainNavigation />
-      <Auth />
+      <Switch>
+        <Route path="/" exact>
+          <Auth />
+        </Route>
+        <Route path="/login/name">
+          <Name />
+        </Route>
+        <Route path="/login/info">
+          <Info />
+        </Route>
+      </Switch>
     </div>
   );
 }
